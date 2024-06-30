6f1c656a6b1ca66fee85c4615a4c8a1c4963be9b
Manish4065 <109040227+Manish4065@users.noreply.github.com>
2024-06-30 14:35:54 +0000

    function scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+function one(){
+    const userName = "manish"
+
+    function two(){
+        const website= "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+
