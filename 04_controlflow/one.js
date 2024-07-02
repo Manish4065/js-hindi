77bea23e7ca3299619db423c4b2a858f82b376e4
Manish4065 <109040227+Manish4065@users.noreply.github.com>
2024-07-02 13:46:09 +0000

    if else

---
+++ 04_controlflow/one.js
@@ -0,0 +1,54 @@
+// if
+
+// if(condition){ // if condition is true then statement1 execute
+//   // statement1;
+// }
+
+const userLoggedin =true
+
+// if(userLoggedin){
+
+// }
+
+// comparision  <, > , >=, <=, !=   === (type chek and equality check)  !==
+
+// if(2 == "2"){
+//     console.log("executed")   //executed
+// }
+
+// if(2 === "2"){
+//     console.log("executed")     // not executed
+
+// }
+
+// temp=50;
+// if(temp==50){
+//     console.log("hi");  // hi
+// }
+
+
+const score =200
+if(score>100){
+    let power="fly"
+    // console.log(`user power : ${power}`)   //user power : fly
+}
+
+// console.log(`user power : ${power}`)     // error
+
+
+const balance=1000
+// if(balance>500) console.log("test1"),console.log("test2");
+
+// nested loop  if, else if, else
+
+
+// and &&, Or || , !
+
+// const userLoggedIn =true
+// const debitCard= true
+// if(userLoggedIn && debitCard){
+//     console.log("allow to buy")
+// }
+
+
+
