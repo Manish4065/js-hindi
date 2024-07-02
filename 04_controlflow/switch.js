fa5492feec68e121b1ec92c8bd36ffff1b53c0de
Manish4065 <109040227+Manish4065@users.noreply.github.com>
2024-07-02 14:03:06 +0000

    switch case

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,23 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month ="feb"
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("february")
+        break;                                                                      
+
+    default:
+        console.log("default case match")
+        break;
+}
