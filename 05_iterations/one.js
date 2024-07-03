639a969e6dfb7557e1168de0a04e63e904023c93
Manish4065 <109040227+Manish4065@users.noreply.github.com>
2024-07-03 06:06:36 +0000

    for, while, do-while loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+let myArray=["flash", "superman", "baltman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
+
+// break and continue
---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// while (condition) {
+    
+// }
+
+// do {
+    
+// } while (condition);
+
+let myArray= ["flash", "batman", "superman"]
+
+let arr=0;
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+}
